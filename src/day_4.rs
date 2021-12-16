deef264dbdbd9cda3aff7b50c75961f7e07d48ea
Jesper Josefsson <jesper.josefsson@paf.com>
2021-12-16 13:35:29 +0200

    It works!

--- src/day_4.rs
+++ src/day_4.rs
@@ -1,5 +1,15 @@
-pub fn day4_part1(_day4_input: &str) -> u32 {
-    0
+use std::slice::Iter;
+
+pub fn day4_part1(day4_input: &str) -> u32 {
+    let (boards, drawn_numbers) = parse_input(day4_input);
+    let maybe_bingo = run_bingo(&boards, &drawn_numbers);
+    maybe_bingo.map_or(0, calculate_result)
+}
+
+fn calculate_result((board, drawn_numbers): BingoResult) -> u32 {
+    let remaining_numbers = unmarked_numbers(board, &drawn_numbers);
+    let last_number = drawn_numbers.last().unwrap_or(&0);
+    remaining_numbers.iter().map(|num| u32::try_from(**num).unwrap_or(0)).sum::<u32>() * u32::try_from(*last_number).unwrap()
 }
 
 type Board = [[u8; 5]; 5];
@@ -11,7 +21,7 @@ type Input = (Vec<Board>, DrawnNumbers);
 fn parse_board(input: &str) -> Board {
     let mut output: Board = [[0; 5]; 5];
     input.lines()
-        .filter(|line| line.len() > 0)
+        .filter(|line| !line.is_empty())
         .map(|row| row.split(' ').filter_map(|n| n.parse::<u8>().ok()))
         .enumerate()
         .for_each(|(index, row)| row.enumerate().for_each(|(col_index, cell)| output[index][col_index] = cell));
@@ -46,7 +56,7 @@ fn has_bingo(board: &Board, drawn_numbers: &[u8]) -> bool {
 fn has_column_bingo(board: &Board, drawn_numbers: &[u8]) -> bool {
     let bingo_column = (0..5)
         .map(|col_index: usize| board.iter()
-            .filter_map(|row| row.get(col_index).map_or(None, |num| Some(num.clone()))).collect::<Vec<u8>>()
+            .filter_map(|row| row.get(col_index).and_then(|num| Some(num.clone()))).collect::<Vec<u8>>()
         ).find(|column| contains_all(&column[..], drawn_numbers));
     bingo_column.is_some()
 }
@@ -60,9 +70,59 @@ fn contains_all(one: &[u8], other: &[u8]) -> bool {
     one.iter().all(|n| other.contains(n))
 }
 
+struct SublistIterator<'a> {
+    output: Vec<u8>,
+    iter: Iter<'a, u8>
+}
+
+impl SublistIterator<'_> {
+    fn new<'a>(origin: &'a[u8]) -> SublistIterator<'a> {
+        SublistIterator {
+            output: Vec::new(),
+            iter: origin.iter()
+        }
+    }
+
+    fn drawn_numbers(self) -> Vec<u8> {
+        self.output.clone()
+    }
+}
+
+impl Iterator for SublistIterator<'_> {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next = self.iter.next();
+        if next.is_some() {
+            self.output.push(*next.unwrap());
+            Some(self.output.clone())
+        } else {
+            None
+        }
+    }
+}
+
+type BingoResult<'a> = (&'a Board, Vec<u8>);
+
+fn run_bingo<'a>(boards: &'a [Board], drawn_numbers: &'_ DrawnNumbers) -> Option<BingoResult<'a>> {
+    let mut iter = SublistIterator::new(drawn_numbers);
+    iter
+        .find_map(|drawn_numbers| boards.iter()
+            .find(|board| has_bingo(board, &drawn_numbers[..]))
+        ).map(|bingo_board| (bingo_board, iter.drawn_numbers()))
+}
+
+fn unmarked_numbers<'a>(board: &'a Board, drawn_numbers: &'_ DrawnNumbers) -> Vec<&'a u8> {
+    board.iter()
+        .flatten()
+        .filter(|num| !drawn_numbers.contains(num))
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day_4::{Board, has_bingo, has_column_bingo, has_row_bingo, parse_board, parse_header, parse_input};
+    use crate::day4_part1;
+    use crate::day_4::{Board, DrawnNumbers, has_bingo, has_column_bingo, has_row_bingo, parse_board, parse_header, parse_input, run_bingo, SublistIterator, unmarked_numbers};
 
     #[test]
     fn test_parse_board() {
@@ -126,16 +186,67 @@ mod tests {
     fn test_has_column_bingo() {
         let board: Board = make_board();
 
-        assert_eq!(false, has_column_bingo(&board, &vec![1,2]));
-        assert_eq!(true, has_column_bingo(&board, &vec![1,6,11,16,21]));
+        assert!(!has_column_bingo(&board, &vec![1,2]));
+        assert!(has_column_bingo(&board, &vec![1,6,11,16,21]));
     }
 
     #[test]
     fn test_has_bingo() {
         let board: Board = make_board();
 
-        assert_eq!(false, has_bingo(&board, &vec![1,2,3]));
-        assert_eq!(true, has_bingo(&board, &vec![6,7,8,9,10]));
-        assert_eq!(true, has_bingo(&board, &vec![2,7,12,17,22]));
+        assert!(!has_bingo(&board, &vec![1,2,3]));
+        assert!(has_bingo(&board, &vec![6,7,8,9,10]));
+        assert!(has_bingo(&board, &vec![2,7,12,17,22]));
+    }
+
+    #[test]
+    fn test_sublist_iterator() {
+        let list: [u8; 5] = [1,2,3,4,5];
+
+        let mut iter = SublistIterator::new(&list);
+
+        assert_eq!(vec![1], iter.next().unwrap());
+        assert_eq!(vec![1,2], iter.next().unwrap());
+        assert_eq!(vec![1,2,3], iter.next().unwrap());
+        assert_eq!(vec![1,2,3,4], iter.next().unwrap());
+        assert_eq!(vec![1,2,3,4,5], iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn test_run_bingo() {
+        let bingo_board = make_board();
+        let no_bingo_board = make_no_bingo_board();
+
+        let boards = vec![bingo_board, no_bingo_board];
+        let should_be_empty = run_bingo(&boards, &vec![1]);
+        assert!(should_be_empty.is_none());
+
+        let should_be_bingo = run_bingo(&boards, &vec![1,2,3,4,5,6]);
+
+        assert!(should_be_bingo.is_some());
+        let (board, drawn_numbers) = should_be_bingo.unwrap();
+        assert_eq!(bingo_board, *board);
+        assert_eq!(vec![1,2,3,4,5], drawn_numbers)
+
+    }
+
+    #[test]
+    fn test_unmarked() {
+        let board = make_board();
+
+        let marked_numbers: DrawnNumbers = (1..=23).collect();
+        let unmarked = unmarked_numbers(&board, &marked_numbers);
+
+        assert_eq!(vec![24,25], unmarked.iter().map(|num| *num.clone()).collect::<Vec<u8>>());
+    }
+
+    #[test]
+    fn test_example() {
+        assert_eq!(4512, day4_part1(include_str!("day4_example.txt")));
+    }
+
+    fn make_no_bingo_board() -> Board {
+        [[99; 5]; 5]
     }
 }
